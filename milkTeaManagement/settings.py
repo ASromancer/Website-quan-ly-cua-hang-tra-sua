589ad244402b86860c8ba0851f3e3624ab3388d6
Nguyễn Tuấn Hiệp <nguyentuanhiep01@gmail.com>
2023-10-17 17:10:44 +0700

    Update settings.py

--- milkTeaManagement/settings.py
+++ milkTeaManagement/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+ALLOWED_HOSTS = []
 
 # Application definition
 
