cb581d47fc0424995c6752a7bf8944769b41cd85
Nguyễn Tuấn Hiệp <nguyentuanhiep01@gmail.com>
2023-10-25 13:44:19 +0700

    update media_root

--- milkTeaManagement/settings.py
+++ milkTeaManagement/settings.py
@@ -85,9 +85,9 @@ DATABASES = {
 #     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 # }
 
-DATABASES = {
-    "default": dj_database_url.parse("postgres://milkteamanagement_user:n3tsK2d0HI7QVc4cevbNhewljXIWkPtj@dpg-cklupuav7m0s73dnue50-a.singapore-postgres.render.com/milkteamanagement")
-}
+# DATABASES = {
+#     "default": dj_database_url.parse("postgres://milkteamanagement_user:n3tsK2d0HI7QVc4cevbNhewljXIWkPtj@dpg-cklupuav7m0s73dnue50-a.singapore-postgres.render.com/milkteamanagement")
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -125,6 +125,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL="/media/"
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
