7acb17b3f95112bd1b76bed57d07c15b519728dd
Nguyễn Tuấn Hiệp <nguyentuanhiep01@gmail.com>
2023-10-25 13:54:05 +0700

    update

--- milkTeaManagement/settings.py
+++ milkTeaManagement/settings.py
@@ -81,9 +81,9 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-# }
+DATABASES = {
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # DATABASES = {
 #     "default": dj_database_url.parse("postgres://milkteamanagement_user:n3tsK2d0HI7QVc4cevbNhewljXIWkPtj@dpg-cklupuav7m0s73dnue50-a.singapore-postgres.render.com/milkteamanagement")
